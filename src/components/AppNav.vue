8e3af137b2d35aad2f95ae0c4e1323c17f888f9a
Sayed2210 <eh600039@gmail.com>
2023-10-19 00:08:06 +0300

    start nav

---
+++ src/components/AppNav.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar
+    ><v-app-bar-nav-icon
+      ><v-icon icon="mdi-home" @click="$router.push({ name: 'home' })"></v-icon
+    ></v-app-bar-nav-icon>
+    <div class="lang d-flex align-center" id="lang-toggle" style="gap: 5px">
+      <span
+        >{{ selected[0].lang }} <v-icon icon="mdi-arrow-down"></v-icon
+      ></span>
+      <v-menu activator="#lang-toggle">
+        <v-list v-model:selected="selected" mandatory>
+          <v-list-item v-for="lang in langs" :key="lang.id" :value="lang">
+            <v-list-item-title>
+              {{ lang.lang }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+    {{ langs[0].dir }}
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    langs: [
+      { id: 2, lang: "Arabic", dir: "rtl" },
+      { id: 1, lang: "English", dir: "ltr" },
+    ],
+    selected: [
+      { id: 2, lang: "Arabic", dir: "rtl" },
+      { id: 1, lang: "English", dir: "ltr" },
+    ],
+  }),
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-layout>
+    <AppNav />
+    <h1>Elsayed</h1>
+  </v-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppNav from "@/components/AppNav.vue";
 
 export default {
-  name: "HomeView",
   components: {
-    HelloWorld,
+    AppNav,
   },
 };
 </script>
